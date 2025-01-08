fc54b5229b0a5f4046de03798c9fbf83d414b456
kimdayeon37 <kdy37912@naver.com>
2025-01-08 15:27:40 +0900

    chore: posts 디렉토리 위치 변경

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,6 +3,7 @@ import type { MetadataRoute } from 'next'
 
 export const baseUrl = 'https://portfolio-blog-starter.vercel.app'
 
+// 블로그가 어떤 구성으로 되어있는지를 쉽게 알려주는 인덱스 파일 => seo 최적화
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   let blogs = getBlogPosts().map((post) => ({
     url: `${baseUrl}/blog/${post.slug}`,
