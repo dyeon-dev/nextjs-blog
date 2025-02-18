c0bd686df64043ec712edd500028055e8a04587e
kimdayeon37 <kdy37912@naver.com>
2025-02-18 17:50:43 +0900

    style: views suspense color

--- app/components/views-skeleton.tsx
+++ app/components/views-skeleton.tsx
@@ -1,5 +1,13 @@
 const ViewsSkeleton = () => {
-  return <div className="h-2 rounded bg-gray-200"></div>;
+  return (
+    <div className="flex animate-pulse space-x-4">
+      <div className="flex-1 space-y-6 py-1">
+        <div className="grid grid-cols-3 gap-4">
+          <div className="col-span-4 h-4 rounded bg-gray-300"></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ViewsSkeleton;
